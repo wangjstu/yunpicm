2ca9c884fb1950c425cdab3f5aa68bb17bdecca9
wangjstu <wangjstu@gmail.com>
2016-12-27 16:10:32 +0100

    revert config

--- common/config/main.php
+++ common/config/main.php
@@ -5,31 +5,31 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=yumpicm',
+            'username' => 'root',
+            'password' => 'sunshine',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'authManager' => [
+            //components数组中加入authManager组件,有PhpManager和DbManager两种方式,
+            //PhpManager将权限关系保存在文件里,这里使用的是DbManager方式,将权限关系保存在数据库.
+            'class' => 'yii\rbac\DbManager'
+        ],
+        'ServiceSupport' =>[  //网站展示工具类
+            'class' => 'common\components\ServiceSupport'
+        ],
+        'PictureTool' => [ //图片相关操作工具类
+            'class' => 'common\components\PictureTool'
+        ]
     ],
-    'db' => [
-        'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=127.0.0.1;dbname=yumpicm',
-        'username' => 'root',
-        'password' => 'sunshine',
-        'charset' => 'utf8',
-    ],
-    'mailer' => [
-        'class' => 'yii\swiftmailer\Mailer',
-        'viewPath' => '@common/mail',
-        // send all mails to a file by default. You have to set
-        // 'useFileTransport' to false and configure a transport
-        // for the mailer to send real emails.
-        'useFileTransport' => true,
-    ],
-    'authManager' => [
-        //components数组中加入authManager组件,有PhpManager和DbManager两种方式,
-        //PhpManager将权限关系保存在文件里,这里使用的是DbManager方式,将权限关系保存在数据库.
-        'class' => 'yii\rbac\DbManager'
-    ],
-    'ServiceSupport' =>[  //网站展示工具类
-        'class' => 'common\components\ServiceSupport'
-    ],
-    'PictureTool' => [ //图片相关操作工具类
-        'class' => 'common\components\PictureTool'
-    ]
 ];
